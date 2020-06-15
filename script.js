951bd5d357c957b02340aa899b4291ad22fdaf26
Robert Gumeny <robert.gumeny528@gmail.com>
2020-06-15 13:10:35 -0600

    just need to syle

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const MODE_BUTTON = document.getElementById("dark-mode")
 
 
-const MAPBOX_STYLE_LIGHT = 'mapbox://styles/rgumeny/ckbgphp5d46y71iqrbjbn2gtc';
-const MAPBOX_STYLE_DARK = 'mapbox://styles/rgumeny/ckbgu000k0hys1ila3gnvd4lg';
-let mapboxStyle = MAPBOX_STYLE_LIGHT;
+const MAPBOX_STYLE_LIGHT = 'mapbox://styles/rgumeny/ckbgphp5d46y71iqrbjbn2gtc/draft';
+const MAPBOX_STYLE_DARK = 'mapbox://styles/rgumeny/ckbgu000k0hys1ila3gnvd4lg/draft';
 let darkmode = false;
 
 let long = -122.335167;
@@ -12,17 +11,24 @@ let lat = 47.608013;
 
 const ACCESS_TOKEN = 'pk.eyJ1Ijoicmd1bWVueSIsImEiOiJja2JncGJ4cHcxN2ptMzVvNDVzYzFnYWYwIn0.eMqsrp118bSjPauhgyxmtA';
 
-if (!darkmode) {
-  mapboxStyle = MAPBOX_STYLE_LIGHT;
-} else {
-  mapboxStyle = MAPBOX_STYLE_DARK;
+function toggleDarkMode() {
+  if (!darkmode) {
+    darkmode = true;
+    map.setStyle(MAPBOX_STYLE_DARK)
+    MODE_BUTTON.innerText = "Light Mode";
+  } else {
+    darkmode = false;
+    map.setStyle(MAPBOX_STYLE_LIGHT)
+    MODE_BUTTON.innerText = "Dark Mode"
+  }
+
 }
 
 
 mapboxgl.accessToken = ACCESS_TOKEN;
 var map = new mapboxgl.Map({
   container: 'map',
-  style: mapboxStyle,
+  style: MAPBOX_STYLE_LIGHT,
   center: [long, lat],
   zoom: 10
 })
@@ -33,3 +39,5 @@ map.addControl(
   })
 );
 map.addControl(new mapboxgl.NavigationControl());
+
+MODE_BUTTON.addEventListener('click', toggleDarkMode)
